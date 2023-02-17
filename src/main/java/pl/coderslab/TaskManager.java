8cf1cdceb909f12c3f931b19594be58c08f81ed5
Adam <adamstepnicky@gmail.com>
2023-02-17 11:08:35 +0100

    Add listOfTasks() method

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,38 @@
 package pl.coderslab;
 
-public class TaskManager {
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        try {
+            listOfTasks();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error while loading list of tasks from the file");
+        }
+    }
+    public static String[][] listOfTasks() throws FileNotFoundException {
+        Path tasks = Paths.get("tasks.csv");
+        if(!Files.exists(tasks)) {
+            throw new FileNotFoundException("File " + tasks + " is missing");
+        }
+        String[][] listOfTasks = new String[0][];
+        String line;
+        try (Scanner scanner = new Scanner(tasks)) {
+            while(scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                listOfTasks = Arrays.copyOf(listOfTasks, listOfTasks.length + 1);
+                listOfTasks[listOfTasks.length - 1] = line.split(",");
+            }
+        } catch (IOException e) {
+            System.err.println("Error while reading file: " + e.getMessage());
+        }
+        return listOfTasks;
+    }
 }
